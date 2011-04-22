07f589729277458d962ba28bc24d71e2873f963d
Littlejth <4512jth@gmail.com>
2011-04-22 10:24:42 -0500

    Fixed common.mk

--- common.mk
+++ common.mk
@@ -22,7 +22,24 @@ DEVICE_PACKAGE_OVERLAYS := device/samsung/common/overlay
 
 # Get additional product configuration from the non-open-source
 # counterpart to this file, if it exists
-$(call inherit-product-if-exists, vendor/samsung/common/common-vendor.mk)
+$(call inherit-product-if-exists, device/samsung/common/common-device.mk)
 
 # Override - we don't want to use any inherited value
 PRODUCT_MANUFACTURER := SAMSUNG
+
+# Key Layouts
+PRODUCT_COPY_FILES += \
+    device/samsung/common/prebuilt/usr/keychars/melfas_ts_input.kcm.bin:system/usr/keychars/melfas_ts_input.kcm.bin \
+    device/samsung/common/prebuilt/usr/keychars/optjoy_device.kcm.bin:system/usr/keychars/optjoy_device.kcm.bin \
+    device/samsung/common/prebuilt/usr/keychars/s3c-keypad-rev0000.kcm.bin:system/usr/keychars/s3c-keypad-rev0050.kcm.bin \
+    device/samsung/common/prebuilt/usr/keychars/s3c-keypad-rev001a.kcm.bin:system/usr/keychars/s3c-keypad-rev0050.kcm.bin \
+    device/samsung/common/prebuilt/usr/keychars/s3c-keypad-rev0010.kcm.bin:system/usr/keychars/s3c-keypad-rev0050.kcm.bin \
+    device/samsung/common/prebuilt/usr/keychars/s3c-keypad-rev0020.kcm.bin:system/usr/keychars/s3c-keypad-rev0050.kcm.bin \
+    device/samsung/common/prebuilt/usr/keychars/s3c-keypad-rev0030.kcm.bin:system/usr/keychars/s3c-keypad-rev0050.kcm.bin \
+    device/samsung/common/prebuilt/usr/keychars/s3c-keypad-rev0040.kcm.bin:system/usr/keychars/s3c-keypad-rev0050.kcm.bin \
+    device/samsung/common/prebuilt/usr/keychars/s3c-keypad-rev0050.kcm.bin:system/usr/keychars/s3c-keypad-rev0050.kcm.bin
+
+PRODUCT_COPY_FILES += \
+    device/samsung/common/prebuilt/usr/keylayout/melfas_ts_input.kl:system/usr/keylayout/melfas_ts_input.kl \
+    device/samsung/common/prebuilt/usr/keylayout/s3c-keypad-rev0050.kl:system/usr/keylayout/s3c-keypad-rev0050.kl \
+    device/samsung/common/prebuilt/usr/keylayout/sec_headset.kl:system/usr/keylayout/sec_headset.kl
