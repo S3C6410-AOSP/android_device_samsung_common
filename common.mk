6c5be9127686b4b78a5e666fdb902000dff07f98
Littlejth <4512jth@gmail.com>
2011-04-22 11:54:55 -0500

    Folder change

--- common.mk
+++ common.mk
@@ -29,17 +29,17 @@ PRODUCT_MANUFACTURER := SAMSUNG
 
 # Key Layouts
 PRODUCT_COPY_FILES += \
-    device/samsung/common/prebuilt/usr/keychars/melfas_ts_input.kcm.bin:system/usr/keychars/melfas_ts_input.kcm.bin \
-    device/samsung/common/prebuilt/usr/keychars/optjoy_device.kcm.bin:system/usr/keychars/optjoy_device.kcm.bin \
-    device/samsung/common/prebuilt/usr/keychars/s3c-keypad-rev0000.kcm.bin:system/usr/keychars/s3c-keypad-rev0050.kcm.bin \
-    device/samsung/common/prebuilt/usr/keychars/s3c-keypad-rev001a.kcm.bin:system/usr/keychars/s3c-keypad-rev0050.kcm.bin \
-    device/samsung/common/prebuilt/usr/keychars/s3c-keypad-rev0010.kcm.bin:system/usr/keychars/s3c-keypad-rev0050.kcm.bin \
-    device/samsung/common/prebuilt/usr/keychars/s3c-keypad-rev0020.kcm.bin:system/usr/keychars/s3c-keypad-rev0050.kcm.bin \
-    device/samsung/common/prebuilt/usr/keychars/s3c-keypad-rev0030.kcm.bin:system/usr/keychars/s3c-keypad-rev0050.kcm.bin \
-    device/samsung/common/prebuilt/usr/keychars/s3c-keypad-rev0040.kcm.bin:system/usr/keychars/s3c-keypad-rev0050.kcm.bin \
-    device/samsung/common/prebuilt/usr/keychars/s3c-keypad-rev0050.kcm.bin:system/usr/keychars/s3c-keypad-rev0050.kcm.bin
+    device/samsung/common/proprietary/usr/keychars/melfas_ts_input.kcm.bin:system/usr/keychars/melfas_ts_input.kcm.bin \
+    device/samsung/common/proprietary/usr/keychars/optjoy_device.kcm.bin:system/usr/keychars/optjoy_device.kcm.bin \
+    device/samsung/common/proprietary/usr/keychars/s3c-keypad-rev0000.kcm.bin:system/usr/keychars/s3c-keypad-rev0050.kcm.bin \
+    device/samsung/common/proprietary/usr/keychars/s3c-keypad-rev001a.kcm.bin:system/usr/keychars/s3c-keypad-rev0050.kcm.bin \
+    device/samsung/common/proprietary/usr/keychars/s3c-keypad-rev0010.kcm.bin:system/usr/keychars/s3c-keypad-rev0050.kcm.bin \
+    device/samsung/common/proprietary/usr/keychars/s3c-keypad-rev0020.kcm.bin:system/usr/keychars/s3c-keypad-rev0050.kcm.bin \
+    device/samsung/common/proprietary/usr/keychars/s3c-keypad-rev0030.kcm.bin:system/usr/keychars/s3c-keypad-rev0050.kcm.bin \
+    device/samsung/common/proprietary/usr/keychars/s3c-keypad-rev0040.kcm.bin:system/usr/keychars/s3c-keypad-rev0050.kcm.bin \
+    device/samsung/common/proprietary/usr/keychars/s3c-keypad-rev0050.kcm.bin:system/usr/keychars/s3c-keypad-rev0050.kcm.bin
 
 PRODUCT_COPY_FILES += \
-    device/samsung/common/prebuilt/usr/keylayout/melfas_ts_input.kl:system/usr/keylayout/melfas_ts_input.kl \
-    device/samsung/common/prebuilt/usr/keylayout/s3c-keypad-rev0050.kl:system/usr/keylayout/s3c-keypad-rev0050.kl \
-    device/samsung/common/prebuilt/usr/keylayout/sec_headset.kl:system/usr/keylayout/sec_headset.kl
+    device/samsung/common/proprietary/usr/keylayout/melfas_ts_input.kl:system/usr/keylayout/melfas_ts_input.kl \
+    device/samsung/common/proprietary/usr/keylayout/s3c-keypad-rev0050.kl:system/usr/keylayout/s3c-keypad-rev0050.kl \
+    device/samsung/common/proprietary/usr/keylayout/sec_headset.kl:system/usr/keylayout/sec_headset.kl
